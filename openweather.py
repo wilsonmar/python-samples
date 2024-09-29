5c00d24418366330293034b327dadabdbc99efd2
Wilson Mar <wilsonmar+github@gmail.com>
2024-09-28 20:19:14 -0600

    v010 move humidty desc left :openweather.py

--- openweather.py
+++ openweather.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-gas "v007 sea_level_hpa as pressure :openweather.py"
+gas "v010 move humidty desc left :openweather.py"
 by Wilson Mar
 LICENSE: MIT
 Based on https://www.instructables.com/Get-Weather-Data-Using-Python-and-Openweather-API/
@@ -11,9 +11,178 @@ and get the API key.
 https://blog.apilayer.com/what-is-the-best-weather-api-for-python/
 """
 from datetime import datetime
+import os
+import pathlib
 import urllib.parse
 import requests
 import math
+
+# Based on: conda install -c conda-forge load_dotenv
+from dotenv import load_dotenv
+# Based on: conda install python-dotenv   # found!
+
+#### Constants used within functions:
+VERBOSE = True  # True or False
+USE_IMPERIAL_UNITS = True  # True or False
+
+use_env_file = True    # -env "python-samples.env"
+global ENV_FILE
+ENV_FILE="python-samples.env"
+
+# Colors
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+# Styles
+BOLD = '\033[1m'
+RESET = '\033[0m'
+
+class bcolors:  # ANSI escape sequences:
+    BOLD = '\033[1m'       # Begin bold text
+    UNDERLINE = '\033[4m'  # Begin underlined text
+
+    HEADING = '\033[37m'   # [37 white
+    FAIL = '\033[91m'      # [91 red
+    ERROR = '\033[91m'     # [91 red
+    WARNING = '\033[93m'   # [93 yellow
+    INFO = '\033[92m'      # [92 green
+    VERBOSE = '\033[95m'   # [95 purple
+    TRACE = '\033[96m'     # [96 blue/green
+                 # [94 blue (bad on black background)
+    CVIOLET = '\033[35m'
+    CBEIGE = '\033[36m'
+    CWHITE = '\033[37m'
+
+    RESET = '\033[0m'   # switch back to default color
+
+def print_separator():
+    """ A function to put a blank line in CLI output. Used in case the technique changes throughout this code. """
+    print(" ")
+
+def print_heading(text_in):
+    if show_heading:
+        if str(show_dates_in_logs) == "True":
+            print('\n***', get_log_datetime(), bcolors.HEADING+bcolors.UNDERLINE,f'{text_in}', bcolors.RESET)
+        else:
+            print('\n***', bcolors.HEADING+bcolors.UNDERLINE,f'{text_in}', bcolors.RESET)
+
+def print_fail(text_in):  # when program should stop
+    if show_fail:
+        if str(show_dates_in_logs) == "True":
+            print('***', get_log_datetime(), bcolors.FAIL, "FAIL:", f'{text_in}', bcolors.RESET)
+        else:
+            print('***', bcolors.FAIL, "FAIL:", f'{text_in}', bcolors.RESET)
+
+def print_error(text_in):  # when a programming error is evident
+    if show_fail:
+        if str(show_dates_in_logs) == "True":
+            print('***', get_log_datetime(), bcolors.ERROR, "ERROR:", f'{text_in}', bcolors.RESET)
+        else:
+            print('***', bcolors.ERROR, "ERROR:", f'{text_in}', bcolors.RESET)
+
+def print_warning(text_in):
+    if show_warning:
+        if str(show_dates_in_logs) == "True":
+            print('***', get_log_datetime(), bcolors.WARNING, f'{text_in}', bcolors.RESET)
+        else:
+            print('***', bcolors.WARNING, f'{text_in}', bcolors.RESET)
+
+def print_todo(text_in):
+    if show_todo:
+        if str(show_dates_in_logs) == "True":
+            print('***', get_log_datetime(), bcolors.CVIOLET, "TODO:", f'{text_in}', bcolors.RESET)
+        else:
+            print('***', bcolors.CVIOLET, "TODO:", f'{text_in}', bcolors.RESET)
+
+def print_info(text_in):
+    if show_info:
+        if str(show_dates_in_logs) == "True":
+            print('***', get_log_datetime(), bcolors.INFO+bcolors.BOLD, f'{text_in}', bcolors.RESET)
+        else:
+            print('***', bcolors.INFO+bcolors.BOLD, f'{text_in}', bcolors.RESET)
+
+def print_verbose(text_in):
+    if show_verbose:
+        if str(show_dates_in_logs) == "True":
+            print('***', get_log_datetime(), bcolors.VERBOSE, f'{text_in}', bcolors.RESET)
+        else:
+            print('***', bcolors.VERBOSE, f'{text_in}', bcolors.RESET)
+
+def print_trace(text_in):  # displayed as each object is created in pgm:
+    if show_trace:
+        if str(show_dates_in_logs) == "True":
+            print('***',get_log_datetime(), bcolors.TRACE, f'{text_in}', bcolors.RESET)
+        else:
+            print('***', bcolors.TRACE, f'{text_in}', bcolors.RESET)
+
+# See https://wilsonmar.github.io/python-samples/#envFile
+
+def open_env_file(env_file) -> str:
+    """Return a Boolean obtained from .env file based on key provided.
+    """
+    from pathlib import Path
+    # See https://wilsonmar.github.io/python-samples#run_env
+    global user_home_dir_path
+    user_home_dir_path = str(Path.home())
+       # example: /users/john_doe
+
+    global_env_path = user_home_dir_path + "/" + env_file  # concatenate path
+
+    # PROTIP: Check if .env file on global_env_path is readable:
+    if not os.path.isfile(global_env_path):
+        print(global_env_path+" (global_env_path) not found!")
+    #else:
+    #    print_info(global_env_path+" (global_env_path) readable.")
+
+    path = pathlib.Path(global_env_path)
+    # Based on: pip3 install python-dotenv
+    from dotenv import load_dotenv
+       # See https://www.python-engineer.com/posts/dotenv-python/
+       # See https://pypi.org/project/python-dotenv/
+    load_dotenv(global_env_path)  # using load_dotenv
+
+    # Wait until variables for print_trace are retrieved:
+    #print_trace("env_file="+env_file)
+    #print_trace("user_home_dir_path="+user_home_dir_path)
+
+def read_env_file():
+    """Read .env file containing variables and values.
+    See https://wilsonmar.github.io/python-samples/#envLoad
+    """
+    # See https://stackoverflow.com/questions/40216311/reading-in-environment-variables-from-an-environment-file
+
+    # Joliet, MT at "45° 29' 5.028'' N-108° 58' 20.244'' W
+
+    global my_latitude
+    my_latitude = get_str_from_env_file('MY_LATITUDE')
+    if my_latitude == None:
+        my_latitude = "34.123"
+        print_warning("my_latitude="+my_latitude+" from default!")
+
+    global my_longitude
+    my_longitude = get_str_from_env_file('MY_LONGITUDE')
+    if my_longitude == None:
+        my_longitude = "104.322"
+        print_warning("my_longitude="+my_longitude+" from default!")
+
+    return
+
+def get_str_from_env_file(key_in) -> str:
+    """Return a value of string data type from OS environment or .env file
+    (using pip python-dotenv)
+    """
+    env_var = os.environ.get(key_in)
+    if not env_var:  # yes, defined=True, use it:
+        print_warning(key_in + " not found in OS nor .env file: " + ENV_FILE)
+        return None
+    else:
+        # PROTIP: Display only first 15 characters of a potentially secret long string:
+        if len(env_var) > 10:
+            print_trace(key_in + "=\"" + str(env_var[:10]) +" (remainder removed)")
+        else:
+            print_trace(key_in + "=\"" + str(env_var) + "\" from .env")
+        return str(env_var)
 
 def get_frost_point_c(t_air_c, dew_point_c):
     """Compute the frost point in degrees Celsius
@@ -85,8 +254,16 @@ def adjust_pressure_for_altitude(sea_level_pressure, altitude, temperature):
 
     # Calculate adjusted pressure
     adjusted_pressure = sea_level_pressure * math.exp(-(GRAVITY * altitude) / (R * T))
-
     return round(adjusted_pressure, 2)
+
+def celcius2fahrenheit(temp_c):
+    return (temp_c * 9/5) + 32
+
+def meters2feet(meters):
+    return meters * 3.28084
+
+def kph2mph(kph):
+    return kph * 3.28084
 
 def compass_text_from_degrees(degrees):
     # adapted from https://www.campbellsci.com/blog/convert-wind-directions
@@ -114,20 +291,104 @@ def compass_text_from_degrees(degrees):
     index = int(round(remainder / 22.5, 0) + 1)   # (17 values)
     return compass_sector[index]
 
+def dew_desc_f(dew_point_f):
+    # Dew Point is the temperature at which air becomes
+    # 100% saturated with water vapor (at 60F=muggy, 70F=humid)
+    # causing condensation to occur.
+    if dew_point_f > 60:
+        return RED+"muggy"+RESET
+    elif dew_point_f > 70:
+        return RED+"humid"+RESET
+    else:
+        return GREEN+"mild"+RESET
 
-#### TODO: Obtain input values from arguments
-VERBOSE = False  # True or False
-USE_IMPERIAL_UNITS = True  # True or False
-city_input = "Billings" # instead of input("Enter City:")
-city_encoded = urllib.parse.quote(city_input)
-apikey="10c5aa3e8279618cfe92220849af5352" # ??? from https://openweathermap.org/api"
+def pressure_desc(pressure):
+    # Above 1022.689 hPa at sea level for clear skies and calm weather.
+    # pressure rarely exceeds 1050 hPa at sea level.
+    if pressure > 1022.689:
+        return RED+"high"+RESET
+    elif pressure < 1010:
+        # pressure below 1010 hPa is often considered Low, associated with unsettled weather conditions.
+        return BLUE+"low"+RESET
+    else:
+        # Normal pressure is between 1009.144 hPa and 1022.689 hPa:
+        return GREEN+"normal"+RESET
 
+def cloud_text(cloud_desc):
+    # https://openweathermap.org/history
+    if cloud_desc == "clear sky":
+        return BLUE+cloud_desc+RESET
+    elif cloud_desc == "few clouds":
+        return BLUE+cloud_desc+RESET
+    elif cloud_desc == "scattered clouds":
+        return BLUE+cloud_desc+RESET
+    elif cloud_desc == "mist":
+        return BLUE+cloud_desc+RESET
+    elif cloud_desc == "broken clouds":
+        return BLUE+cloud_desc+RESET
+
+    elif cloud_desc == "shower rain":
+        return GREEN+cloud_desc+RESET
+    elif cloud_desc == "rain":
+        return GREEN+cloud_desc+RESET
+
+    elif cloud_desc == "thunderstorm":
+        return RED+cloud_desc+RESET
+    elif cloud_desc == "snow":
+        return RED+cloud_desc+RESET
+
+    # "01d" or "01n": Clear sky (day or night)
+    # "02d" or "02n": Few clouds
+    # "03d" or "03n": Scattered clouds
+    # "04d" or "04n": Broken clouds
+    # "09d" or "09n": Shower rain
+    # "10d" or "10n": Rain
+    # "11d" or "11n": Thunderstorm
+    # "13d" or "13n": Snow
+    # "50d" or "50n": Mist
+    # const iconCode = weatherData.weather[0].icon;
+    # const iconUrl = `https://openweathermap.org/img/wn/${icon_Code}@2x.png`;
+    # document.getElementById('weather-icon').src = iconUrl;
+
+###############
+
+if __name__ == "__main__":
+
+    # PROTIP: Global variable referenced within functions:
+    # values obtained from .env file can be overriden in program call arguments:
+    show_fail = True       # Always show
+    show_error = False      # Always show
+    show_warning = False    # -wx  Don't display warning
+    show_todo = False       # -td  Display TODO item for developer
+    show_info = False       # -qq  Display app's informational status and results for end-users
+    show_heading = False    # -q  Don't display step headings before attempting actions
+    show_verbose = False    # -v  Display technical program run conditions
+    show_trace = False      # -vv Display responses from API calls for debugging code
+    show_secrets = False   # Never show
+
+    show_sys_info = False
+    show_config = False   # not used?
+
+    global show_dates_in_logs
+    show_dates_in_logs = False
+
+    open_env_file(ENV_FILE)
+    read_env_file()  # calls print_samples()
+
+    openweathermap_api_key = get_str_from_env_file('OPENWEATHERMAP_API_KEY')
+    if not openweathermap_api_key:
+       print("OPENWEATHERMAP_API_KEY has no default! Processing killed")
+
+apispec="lat=" + my_latitude +"&lon=" + my_longitude
+# https://api.openweathermap.org/data/2.5/weather?lat=40.7128&lon=-74.0060&appid={API key}
 #url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=" + apikey + "&units=metric.format(city)"
-url = "http://api.openweathermap.org/data/2.5/weather?q="+city_encoded+"&appid=" + apikey
+url = "http://api.openweathermap.org/data/2.5/weather?"+apispec+"&appid="+openweathermap_api_key
+#city_input = "q=Billings" # instead of input("Enter City:")
+#city_encoded = urllib.parse.quote(city_input)
 
-if VERBOSE:
-   print(">>> url=",url)
-   # data= {'coord': {'lon': 23.886, 'lat': 48.1792}, 'weather': [{'id': 803, 'main': 'Clouds', 'description': 'broken clouds', 'icon': '04d'}], 'base': 'stations', 'main': {'temp': 296.48, 'feels_like': 296.28, 'temp_min': 296.48, 'temp_max': 296.48, 'pressure': 1012, 'humidity': 54, 'sea_level': 1012, 'grnd_level': 938}, 'visibility': 10000, 'wind': {'speed': 2.43, 'deg': 235, 'gust': 6.41}, 'clouds': {'all': 75}, 'dt': 1727441174, 'sys': {'country': 'UA', 'sunrise': 1727410696, 'sunset': 1727453543}, 'timezone': 10800, 'id': 709516, 'name': 'Dubove', 'cod': 200}
+#if VERBOSE:
+#   print(">>> url=",url)  # contains APIKEY
+   # data= {'coord': {'lon': -108.975, 'lat': 45.4869}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01d'}], 'base': 'stations', 'main': {'temp': 302.63, 'feels_like': 301.72, 'temp_min': 299.05, 'temp_max': 302.63, 'pressure': 1009, 'humidity': 34, 'sea_level': 1009, 'grnd_level': 879}, 'visibility': 10000, 'wind': {'speed': 3.28, 'deg': 121, 'gust': 4.99}, 'clouds': {'all': 0}, 'dt': 1727569281, 'sys': {'type': 2, 'id': 2006447, 'country': 'US', 'sunrise': 1727529056, 'sunset': 1727571699}, 'timezone': -21600, 'id': 5661766, 'name': 'Laurel', 'cod': 200}
 try:
     res = requests.get(url)
     data = res.json()
@@ -139,7 +400,7 @@ except Exception as e:
 current_datetime = datetime.now()
 
 if VERBOSE:
-    print(">>> data=",data)
+    print(">>> returned data=",data)
 
 
 # Extract individual elements:
@@ -148,13 +409,13 @@ lat = data['coord']['lat']
 try:
     station_name = data['sys']['name']
 except Exception as e:
-    print(f">>> Error getting: name in {e}")
+    # print(f">>> Error getting: name in {e}")
     station_name = ""
     # exit()
 else:
     station_name = ""
 
-description = data['weather'][0]['description']
+cloud_desc = data['weather'][0]['description']
 icon_code = data['weather'][0]['icon']  # '01n'
 # 'sys': {'country': 'UA', 'sunrise': 1727497180, 'sunrise': 1727539818},
 country = data['sys']['country']
@@ -177,24 +438,10 @@ grnd_level_hpa = data['main']['grnd_level']
 pressure = data['main']['pressure']
 
 
-#### Icon for GUI (not CLI):
-# icon_code =
-   # "01d" or "01n": Clear sky (day or night)
-   # "02d" or "02n": Few clouds
-   # "03d" or "03n": Scattered clouds
-   # "04d" or "04n": Broken clouds
-   # "09d" or "09n": Shower rain
-   # "10d" or "10n": Rain
-   # "11d" or "11n": Thunderstorm
-   # "13d" or "13n": Snow
-   # "50d" or "50n": Mist
-# const iconCode = weatherData.weather[0].icon;
-# const iconUrl = `https://openweathermap.org/img/wn/${icon_Code}@2x.png`;
-# document.getElementById('weather-icon').src = iconUrl;
+#### Calculations from response:
 
-
-#### Calculations:
 STRFTIME_FORMAT="%I:%M %p (%H:%M:%S) %Y-%m-%d"
+cloud_text = cloud_text(cloud_desc)
 formatted_datetime=current_datetime.strftime(STRFTIME_FORMAT)
 sunrise_date_time = datetime.fromtimestamp(sunrise_epoch)
 sunrise_formatted = sunrise_date_time.strftime(STRFTIME_FORMAT)
@@ -205,24 +452,8 @@ temp_c = float(temp_k) - 273.15
 # Dew point provides a more consistent and
 # easily interpretable measure of how humid it actually feels outside.
 dew_point_c = get_dew_point_c(temp_c, humidity)
-dew_point_f = (dew_point_c * 9/5) + 32
-# Dew Point is the temperature at which air becomes
-# 100% saturated with water vapor (at 60F=muggy, 70F=humid)
-# causing condensation to occur.
-if dew_point_f > 60:
-    dew_comfort = "muggy"
-elif dew_point_f > 70:
-    dew_comfort = "humid"
-else:
-    dew_comfort = "mild"
-
-# TODO: More categories of comfort, localized?
-if pressure > 60:
-    pressure_desc = "falling"
-elif pressure > 70:
-    pressure_desc = "rising"
-else:
-    pressure_desc = "steady"
+dew_point_f = celcius2fahrenheit(dew_point_c)
+dew_comfort = dew_desc_f(dew_point_f)
 
 
 #### Print:
@@ -230,22 +461,22 @@ else:
 print(f"openweather.org at {formatted_datetime} reports")  # cloud, rain
 print(f"          Sunrise: {sunrise_formatted}")
 print(f"          Sunset:  {sunset_formatted}")
-print(f"{description} at \"{city_input}\"",end="")
+print(f"{cloud_text} at \"{apispec}\"",end="")
 print(f" country={country}",end="")
 if station_name == "":
     print("")
 else:
     print(f" ({station_name})")
 
-# _ft = _m * 3.28084
-print(f"    Latitude:  {lat} from the Equator &")
-print(f"    Longitude: {lon} from the Meridian at Greenwich, UK")
+print(f"    Latitude:  {my_latitude} from the Equator &")
+print(f"    Longitude: {my_longitude} from the Meridian at Greenwich, UK")
 # Not print if same: print('Feels like: ',feels_like)
 
 if USE_IMPERIAL_UNITS:
-   temp_f = (temp_c * 9/5) + 32
-   print(f"{temp_f:.2f}°F",end="")
-   print(f" with {dew_comfort} Humidity: {humidity}%",end="")
+   temp_f = celcius2fahrenheit(temp_c)
+   dew_point_c
+   print(f"{dew_comfort} humidity {humidity}%",end="")
+   print(f" at {BOLD}{temp_f:.2f}°F{RESET}",end="")
    print(f" for Dew Point: {dew_point_f:.2f}°F")
 else:
    print(f"At a {dew_comfort} {temp_c:.2f}°C",end="")
@@ -255,7 +486,7 @@ else:
 # Convert wind direction to names (such as NWW):
 wind_dir = compass_text_from_degrees(wind_deg)
 if USE_IMPERIAL_UNITS:
-    wind_mph=wind_kph * 0.621371
+    wind_mph = kph2mph(wind_kph)
     print(f"Wind: {wind_mph:.2f} mph from {wind_dir} ({wind_deg}°)",end="")
 else:
     print(f"Wind: {wind_kph:.2f} kph from {wind_dir} ({wind_deg}°)",end="")
@@ -264,10 +495,11 @@ if wind_gust_kph == 0:
     print("")
 else:
     if USE_IMPERIAL_UNITS:
-        wind_gust_mph = wind_gust_kph * 0.621371
+        wind_gust_mph = kph2mph(wind_gust_kph)
         print(f" with gusts: {wind_gust_mph:.2f} mph")
     else:
         print(f" with gusts: {wind_gust_kph:.2f} kph")
+
 
 # sea_level returned is a normalized value that allows for comparison between different locations, regardless of their actual elevation.
 # The average sea-level pressure is 1013.25 mb/hPA (Hectopascal).
@@ -285,10 +517,13 @@ altitude_to_adjust = sea_level_hpa  # meters
 adjusted_pressure = adjust_pressure_for_altitude \
     (sea_level_hpa, altitude_to_adjust, temp_c)
 
-# Format & display each element:
+#### Format & display each element:
 
-print(f"Atmospheric pressure: {sea_level_hpa} hPa (Hectopascals, aka millibars)")
-print(f"       (Ground_level:  {grnd_level_hpa} hPa)")
+pressure_desc = pressure_desc(pressure)
+print(f"{RED}{pressure_desc}{RESET} pressure at {sea_level_hpa} hPa (Hectopascals, aka millibars)")
+print(f"      (vs. normal: 1013.25 hPa at sea level)")
+print(f"    (Ground_level:  {grnd_level_hpa} hPa)")
+# Pressure changes with altitude, decreasing by about 12 hPa for every 100 meters of elevation.
 
 #print(f"is {pressure_diff:.2f} \
 #      hPa from adjusted average \
@@ -297,11 +532,4 @@ print(f"       (Ground_level:  {grnd_level_hpa} hPa)")
 # print(f"{hectopascals:.2f} hPa above sea level average of 1013.25 hPA")
 # What is normal for your location's altitude
 # Python code: https://www.perplexity.ai/search/how-adjust-normal-hectopascals-SuInNLlARxadL9GbY.NmBA
-# print(f"      vs. 1013.25 hPa unadjusted normally at sea level.")
 # print(f"      vs. {adjusted_pressure:.2f} hPa normally for local elevation.")
-
-# Normal pressure is between 1009.144 hPa and 1022.689 hPa.
-# Above 1022.689 hPa at sea level for clear skies and calm weather.
-# rarely exceeds 1050 hPa at sea level.
-# Pressure changes with altitude, decreasing by about 12 hPa for every 100 meters of elevation.
-# pressure below 1010 hPa is often considered Low, associated with unsettled weather conditions.
