d93de4ff2ead0dd573441d1c6f76812afb480c1c
Wilson Mar <wilsonmar+github@gmail.com>
2025-01-10 08:41:58 -0700

    v002 + Walmart foods :foods.py

--- foods.py
+++ foods.py
@@ -1,10 +1,11 @@
-foods.py
+#!/usr/bin/env python3
 
-"""foods.py at 
+"""foods.py at https://github.com/wilsonmar/python-samples/blob/main/foods.py
 STATUS: In design.
-"v001 + new :foods.py"
+"v002 + Walmart foods :foods.py"
 
-This program identifies complete meal plans for specific conditions such as low sodium.
+This program identifies complete meal plans for specific conditions (such as low sodium and potassium)
+by making APIs and read/write to a local database.
 
 https://spoonacular.com/food-api
 https://www.youtube.com/watch?v=V20Mj4w-7K4
@@ -12,5 +13,11 @@ https://www.youtube.com/watch?v=UxcdwEOCD48&t=33s = Fun APIs
 https://www.postman.com/spoonacular-api/spoonacular-api/collection/rqqne3j/spoonacular-api
 https://www.postman.com/spoonacular-api/spoonacular-api/api/4b798e92-89ad-4638-89d2-02fc2ee45497
 https://www.reddit.com/r/tasker/comments/h7pkdu/api_whats_for_dinner_tonight_spoonacular_has_you/
+
+No/Low Salt items at Walmart:
+* No Salt Chicken Stock
+* Hint of Salt Ritz crackers
+
+
 """
 
