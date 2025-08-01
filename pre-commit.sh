111e13f018a37f5c773b09ffb74ea313be0f4669
Wilson Mar <wilsonmar+github@gmail.com>
2025-08-01 03:38:06 -0600

    v005 + safety check to scan :pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-# git commit -m"v001 + new :pre-commit.sh"
+# gxp "v005 + safety check to scan :pre-commit.sh"
 
 # This is from https://github.com/wilsonmar/mac-setup/blob/main/pre-commit.sh
+# Explained at https://wilsonmar.github.io/python-scans
 # to scan only files which have been git commit applied.
 
 # At your repo's folder (where its .git is listed):
 # Copy this file to your repo's .git/hooks/pre-commit  # file name without an extension
+# cp ../mac-setup/pre-commit.sh .
 # cp pre-commit.sh .git/hooks/pre-commit
 # chmod +x .git/hooks/pre-commit
 # echo "\n#" >> temp1.py
 # git add temp1.py;git commit -m"test temp1.py"
 
+START=$(date +%s)
+#START=$(date +%s%3N)  # for millisecond precision
+handle_error() {
+   #END=$(date +%s%3N)
+   END=$(date +%s)
+   DURATION=$((END - START))
+   echo "Elapsed Time: $DURATION milliseconds"
+   exit 1
+}
+trap 'handle_error' ERR
+
 echo "Running pre-commit.sh from .git/hooks/pre-commit"
-exit
 
 set -eo pipefail
 
@@ -21,10 +33,10 @@ set -eo pipefail
 # if ! command -v flake8 >/dev/null; then  # command not found, so:
 # brew install ggshield
 
-brew install safety
-safety check
+# pipx install pip-audit
+safety scan  # not used because its deprecated.
 if [ $? -ne 0 ]; then
-    echo "Safety found vulnerabilities found. Commit aborted."
+    echo "Safety scan found vulnerabilities found. Commit aborted."
     exit 1
 fi
 
@@ -34,19 +46,25 @@ PY_FILES=$(echo "$CHANGED_FILES" | grep '\.py$' || true)
 if [[ -n "$PY_FILES" ]]; then
    # black --check $PY_FILES
    brew install ruff  # instead of flake8 & black
+   echo "ruff check $PY_FILES"
    ruff check $PY_FILES   # instead of . for all files
-fi
-if [ $? -ne 0 ]; then
-   echo "Ruff found vulnerabilities. Commit aborted."
-   exit 1
+   if [ $? -ne 0 ]; then
+      echo "Ruff found vulnerabilities. Commit aborted."
+      exit 1
+   fi
+
+   #brew install bandit
+   ## instead of . for all files, including transitive dependencies:
+   #bandit -r $PY_FILES
+   #if [ $? -ne 0 ]; then
+   #   echo "Bandit found vulnerabilities. Commit aborted."
+   #   exit 1
+   #fi
 fi
 
-brew install bandit
-# instead of . for all files, including transitive dependencies:
-bandit -r $PY_FILES
-if [ $? -ne 0 ]; then
-    echo "Bandit found vulnerabilities. Commit aborted."
-    exit 1
-fi
+END=$(date +%s)
+DURATION=$((END - START))
+echo "pre-commit.sh elapsed Time: $DURATION seconds"
+
 
 # brew install snyk-cli
