3cc9709d597e8db52b848658e61ca8c030f6af1e
Wilson Mar <wilsonmar+github@gmail.com>
2025-09-15 14:33:10 -0600

    25-09-15 v006 + ruff check clear :calculator-tk.py

--- calculator-tk.py
+++ calculator-tk.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
-"""calculator-tk.py at https://github.com/wilsonmar/python-samples/blob/main/calculator-tk.py
-Provides a calculator created using the Tk library for Python.
+r"""calculator-tk.py here.
+
+at https://github.com/wilsonmar/python-samples/blob/main/calculator-tk.py
+
+Provides a calculator created using the Tk GUI library for Python.
 
 gas "v003 + try :calculator-tk.py"
 STATUS: Not working. Output env: python3\r: No such file or directory
-   
+
 Before running, in CLI run pip install tkinter
+
 """
+
+__last_change__ = "25-09-15 v006 + ruff check clear :calculator-tk.py"
+__status__ = "works great."
+
 import tkinter as tk
 
 # Create Window
@@ -22,28 +30,34 @@ current_text.set("0")
 
 # Will contain properties and logic for calculation only
 class Calculator:
+    """Calculator class."""
+
     def __init__(self):
         self.last_number = 0
         self.current_operation = ""
         self.current_number = 0
 
     def set_operation(self, operation):
+        """Set operator."""
         self.current_operation = operation
         self.last_number = self.current_number
         self.current_number = 0
 
     def clear_all(self, text_box):
+        """Clear all."""
         self.current_operation = ""
         self.last_number = 0
         self.current_number = 0
         text_box.set("0")
 
     def set_number(self, num, text_box):
+        """Set number."""
         new_num = str(self.current_number) + str(num)
         self.current_number = int(new_num)
         text_box.set(self.current_number)
 
     def calculate(self, text_box):
+        """Calculate."""
         match self.current_operation:
             case "+":
                 self.last_number = self.last_number + self.current_number
@@ -60,7 +74,7 @@ class Calculator:
 # in order to have our own usable copy of the class
 calc = Calculator()
 
-#Input window and Title
+# Input window and Title
 display_frame = tk.Frame(window)
 tk.Label(display_frame, text="Python Calculator", font=("Georgia", 25), anchor=tk.CENTER).pack()
 output = tk.Entry(display_frame, justify=tk.RIGHT, width=45, font=16, state="disabled", textvariable=current_text)
@@ -72,6 +86,7 @@ button_frame = tk.Frame(window)
 
 # Function avoid duplicating the style information
 def create_button(text, command=None):
+    """Create button."""
     return tk.Button(button_frame, text=text, height=4, width=10, font=20, command=command)
 
 
@@ -89,15 +104,13 @@ create_button(9, command=lambda: calc.set_number(9, current_text)).grid(row=1, c
 create_button(4, command=lambda: calc.set_number(4, current_text)).grid(row=2, column=0)
 create_button(5, command=lambda: calc.set_number(5, current_text)).grid(row=2, column=1)
 create_button(6, command=lambda: calc.set_number(6, current_text)).grid(row=2, column=2)
-create_button("-", command=lambda: calc.set_operation("-"))\
-    .grid(row=2, column=3)
+create_button("-", command=lambda: calc.set_operation("-")).grid(row=2, column=3)
 
 # Fourth Row
 create_button(1, command=lambda: calc.set_number(1, current_text)).grid(row=3, column=0)
 create_button(2, command=lambda: calc.set_number(2, current_text)).grid(row=3, column=1)
 create_button(3, command=lambda: calc.set_number(3, current_text)).grid(row=3, column=2)
-create_button("+", command=lambda: calc.set_operation("+"))\
-    .grid(row=3, column=3)
+create_button("+", command=lambda: calc.set_operation("+")).grid(row=3, column=3)
 
 # Fifth Row
 # create_button("+/-").grid(row=4, column=0)
