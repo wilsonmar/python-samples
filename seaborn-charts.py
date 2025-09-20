71a6c683b569d4a1f41c082a2f8dce01e4c3ee67
Vijayakumar Durairaj <vijaykumar_cnp@yahoo.co.in>
2025-09-20 19:03:28 +0530

    comments incorporated

--- seaborn-charts.py
+++ seaborn-charts.py
@@ -1,6 +1,9 @@
 """Create chart for the data."""
 
-__last_change__ = "25-09-20 v001 + regression line,dynamic size of points added :seaborn-charts.py"
+__last_change__ = "25-09-20 v002 + grey dotted line,time stamp,Accuracy verbiage added :seaborn-charts.py"
+
+# Internal imports (no pip/uv add needed):
+from datetime import datetime, timezone
 
 try:
     import pandas as pd
@@ -21,17 +24,31 @@ plt.figure(figsize=(10, 6))
 #Create a Seaborn lineplot
 # Ensure 'x_column' and 'y_column' exist in your CSV file
 sns.set_theme(style='darkgrid')
+
 # Get the colors from a Seaborn palette
-palette = sns.color_palette("viridis", as_cmap=False, n_colors=len(df['Output'].unique()))
+palette = sns.color_palette("viridis", as_cmap=False, n_colors=len(df['Accuracy'].unique()))
 
 # Map the 'z' column to the colors
-color_map = {category: color for category, color in zip(df['Output'].unique(), palette)}
-edge_colors = df['Output'].map(color_map).values
+color_map = {category: color for category, color in zip(df['Accuracy'].unique(), palette)}
+edge_colors = df['Accuracy'].map(color_map).values
 
+#Get the current UTC time
+current_utc_time = datetime.now(timezone.utc)
+formatted_time = current_utc_time.strftime('%Y-%m-%d %H:%M:%S UTC')
+# The 'transform=plt.gca().transAxes' places the text relative to the axes
+plt.text(
+    0.95,  # x-position (0.0 to 1.0)
+    0.95,  # y-position (0.0 to 1.0)
+    formatted_time,
+    horizontalalignment='right',
+    verticalalignment='bottom',
+    transform=plt.gca().transAxes,
+    fontsize=8,
+    color='grey'
+)
 #change edge color and size of marker by chaging s values
-
-ax=sns.scatterplot(data=df, x='MilliSecs', y=' USD cents ',markers=True,ls='-',color='cornflowerblue',hue='Output',legend='auto',edgecolor='black',sizes=(50,200),size='MilliSecs')
-ax=sns.regplot(data=df, x='MilliSecs', y=' USD cents ', ax=ax, scatter=False, ci=None, color='red')
+ax=sns.scatterplot(data=df, x='MilliSecs', y='USD cents',markers=True,ls='-',color='cornflowerblue',hue='Accuracy',legend='auto',edgecolor='black',sizes=(50,200),size='MilliSecs')
+ax=sns.regplot(data=df, x='MilliSecs', y='USD cents', ax=ax, scatter=False, ci=None, color='grey',line_kws={'linestyle': '--'})
 #Add plot titles and labels for clarity
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 #Customize the Legend
